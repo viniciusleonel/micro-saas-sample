3418886d3bfe643eb596fe3f05f78ba768084308
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-06-13 15:27:37 -0300

    fix: corrigindo var mailtrap, recebendo email da pag auth no mailtrap

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,8 +1,7 @@
 import NextAuth from "next-auth";
-import EmailProvider from "next-auth/providers/email";
+import EmailProvider from "next-auth/providers/nodemailer";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "../database";
-
 
 export const { 
     handlers: { GET, POST},
@@ -12,9 +11,14 @@ export const {
     providers: [
         EmailProvider({
             server: {
-                server: process.env.EMAIL_SERVER,
-                from: process.env.EMAIL_FROM,
-            }
+                host: process.env.EMAIL_SERVER,
+                port: process.env.EMAIL_PORT,
+                auth: {
+                    user: process.env.EMAIL_USER,
+                    pass: process.env.EMAIL_PASSWORD,
+                },
+            },
+            from: process.env.EMAIL_FROM,
         })
     ],
 });
